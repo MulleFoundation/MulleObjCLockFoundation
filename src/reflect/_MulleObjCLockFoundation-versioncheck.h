924261552784445c85854350dbbac8eedfb5e3cd
Nat! <nat@mulle-kybernetik.com>
2024-01-22 18:07:34 +0100

    update project and version info

--- src/reflect/_MulleObjCLockFoundation-versioncheck.h
+++ src/reflect/_MulleObjCLockFoundation-versioncheck.h
@@ -5,21 +5,6 @@
 #ifndef mulle_objc_lock_foundation_versioncheck_h__
 #define mulle_objc_lock_foundation_versioncheck_h__
 
-#if defined( MULLE_OBJC_VERSION)
-# ifndef MULLE_OBJC_VERSION_MIN
-#  define MULLE_OBJC_VERSION_MIN  ((0UL << 20) | (23 << 8) | 0)
-# endif
-# ifndef MULLE_OBJC_VERSION_MAX
-#  define MULLE_OBJC_VERSION_MAX  ((0UL << 20) | (24 << 8) | 0)
-# endif
-# if MULLE_OBJC_VERSION < MULLE_OBJC_VERSION_MIN
-#  error "MulleObjC is too old"
-# endif
-# if MULLE_OBJC_VERSION >= MULLE_OBJC_VERSION_MAX
-#  error "MulleObjC is too new"
-# endif
-#endif
-
 #if defined( MULLE_OBJC_TIME_FOUNDATION_VERSION)
 # ifndef MULLE_OBJC_TIME_FOUNDATION_VERSION_MIN
 #  define MULLE_OBJC_TIME_FOUNDATION_VERSION_MIN  ((0UL << 20) | (1 << 8) | 2)
